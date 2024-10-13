4946cd55e7ed9fa0447bd029fa2102318182ddac
TonyCross23 <tonychamp40@gmail.com>
2024-10-13 19:18:46 +0630

    drap

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -2,13 +2,15 @@ import React, { useMemo, useState } from "react";
 import { CiCirclePlus } from "react-icons/ci";
 import { Column, Id } from "../type/types";
 import ColumnContainer from "./columnContainer";
-import {DndContext} from '@dnd-kit/core';
-import {SortableContext} from '@dnd-kit/sortable';
+import {DndContext, DragEndEvent, DragOverlay, DragStartEvent, PointerSensor, useSensor, useSensors} from '@dnd-kit/core';
+import {arrayMove, SortableContext} from '@dnd-kit/sortable';
+import { createPortal } from "react-dom";
 
 const Board: React.FC = () => {
 
   const [columns, setColumn ] = useState<Column[]>([])
   const columnId = useMemo(() =>columns.map((column) => column.id),[columns]);
+  const [activeColumn, setActiveColumn] = useState<Column | null>(null)
   
 
   const createNewColumn = () => {
@@ -28,9 +30,47 @@ const Board: React.FC = () => {
     const filterColumns = columns.filter((column) => column.id !== id)
     setColumn(filterColumns)
   }
+
+  const sensor = useSensors(
+    useSensor(PointerSensor,{
+      activationConstraint: {
+        distance: 3,
+      }
+    })
+  )
+
+  const onDragStart = (event: DragStartEvent) => {
+    if (event.active.data.current?.type === 'Column') {
+      setActiveColumn(event.active.data.current.column);
+      return;
+    }
+  }
+
+  const onDrageEnd = (event: DragEndEvent) => {
+      const {active, over} = event;
+      if (!over) return;
+
+      const activeColumnId = active.id;
+      const overColumnId = over.id;
+
+      if(activeColumnId === overColumnId) return;
+
+      setColumn((columns) => {
+        const activeColumnIndex = columns.findIndex(
+          (column) => column.id === activeColumnId
+        );
+
+        const overColumnIndex = columns.findIndex(
+          (column) => column.id === overColumnId
+        );
+
+        return arrayMove(columns, activeColumnIndex, overColumnIndex)
+      })
+  }
+
   return (
     <div className="mx-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
-   <DndContext>
+   <DndContext sensors={sensor} onDragStart={onDragStart} onDragEnd={onDrageEnd}>
       <div className="m-auto flex gap-4">
             <div className="flex flex-wrap gap-4 my-10">
               <SortableContext items={columnId}>
@@ -43,6 +83,17 @@ const Board: React.FC = () => {
               border-columnBackGroundColor p-4 ring-red-500 hover:ring-2 flex gap-2 m-auto">
                <CiCirclePlus className="text-xl"/> Add Column
             </button>
+            {createPortal(<DragOverlay>
+              {
+                activeColumn && (
+                  <ColumnContainer
+                    column={activeColumn}
+                    deleteColumn={delectColumn}
+                  />
+                )
+              }
+            </DragOverlay>,
+            document.body)}
       </div>
    </DndContext>
     </div>
--- src/components/columnContainer.tsx
+++ src/components/columnContainer.tsx
@@ -11,7 +11,7 @@ interface Props {
 const ColumnContainer = (props: Props) => {
   const { column, deleteColumn } = props;
 
-  const {setNodeRef, attributes, listeners, transform, transition} = useSortable({
+  const {setNodeRef, attributes, listeners, transform, transition, isDragging} = useSortable({
     id: column.id,
     data: {
       type: "Column",
@@ -22,6 +22,14 @@ const ColumnContainer = (props: Props) => {
   const style = {
     transition,
     transform: CSS.Transform.toString(transform),
+  }
+
+  if(isDragging) {
+    return (
+      <div ref={setNodeRef} style={style} className="bg-columnBackGroundColor w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col opacity-40 border-2 border-red-500">
+
+      </div>
+    );
   }
 
   return (
